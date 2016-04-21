ea9843731936a87ae2c6ef225a2ea24cfd604d9a
Jordan <Jordan@ADVP-JYOUNG>
2016-04-21 09:46:17 -0300

    Add project files.

---
+++ MyTinyCollege/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyTinyCollege.Startup))]
+namespace MyTinyCollege
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
